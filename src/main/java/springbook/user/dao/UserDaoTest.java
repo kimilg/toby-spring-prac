5216c638c6382d5f0bedb4351350e64aaa538868
ilgoo.kim <ilgoo.kim@navercorp.com>
2022-05-28 18:58:48 +0900

    Factory 클래스 대신 applicationContext.xml 파일을 이용해서 빈 등록

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -3,10 +3,12 @@ package springbook.user.dao;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.util.Assert;
 import springbook.user.dao.connection.CountingConnectionMaker;
@@ -15,37 +17,47 @@ import springbook.user.domain.User;
 import java.sql.SQLException;
 
 public class UserDaoTest {
-    public static void main(String[] args){
-        JUnitCore.main("springbook.user.dao.UserDaoTest");
+//    public static void main(String[] args){
+//        JUnitCore.main("springbook.user.dao.UserDaoTest");
+//    }
+    
+    private UserDao dao;
+    private User user1;
+    private User user2;
+    private User user3;
+    
+    @Before
+    public void setup(){
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        this.dao = context.getBean("userDao", UserDao.class);
+        this.user1 = new User("gyumee", "박성철", "springno1");
+        this.user2 = new User("leegw700", "이길원", "springno2");
+        this.user3 = new User("bumjin", "박범진", "springno3");
     }
     
     @Test
     public void addAndGet() throws ClassNotFoundException, SQLException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
         
-        User user = new User("kimig2", "holyman", "yeap");
-
-        dao.add(user);
+        dao.add(user1);
         assertThat(dao.getCount(), is(1));
 
-        User user2 = dao.get(user.getId());
-        
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassword(), is(user.getPassword()));
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
-        System.out.println(ccm.getCounter());
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
+        
+        User user = dao.get(user1.getId());
+        
+        assertThat(user1.getName(), is(user.getName()));
+        assertThat(user1.getPassword(), is(user.getPassword()));
+
     }
     
     @Test(expected= EmptyResultDataAccessException.class)
     public void getUserFailure() throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
         
