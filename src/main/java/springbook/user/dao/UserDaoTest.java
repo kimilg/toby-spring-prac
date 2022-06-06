955e3d3ae3cc7c7840956b5affe8b63fa11e31d3
ilgoo.kim <ilgoo.kim@navercorp.com>
2022-06-06 17:50:38 +0900

    jdbcTemplate 적용 - RuntimeException 직접 제작

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -1,6 +1,7 @@
 package springbook.user.dao;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -9,11 +10,15 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -26,6 +31,9 @@ public class UserDaoTest {
     
     @Autowired
     private UserDao dao;
+    
+    @Autowired
+    DataSource dataSource;
     
     private User user1;
     private User user2;
@@ -43,6 +51,22 @@ public class UserDaoTest {
     }
     
     @Test
+    public void sqlExceptionTranslate() {
+        dao.deleteAll();
+        try {
+            dao.add(user1);
+            dao.add(user1);
+        }
+        catch(DuplicateKeyException ex) {
+            SQLException sqlEx = (SQLException) ex.getRootCause();
+            SQLExceptionTranslator set = 
+                new SQLErrorCodeSQLExceptionTranslator(this.dataSource);
+            
+            assertThat(set.translate(null, null, sqlEx).getClass(), is(DuplicateKeyException.class));
+        }
+    }
+    
+    @Test(expected = DuplicateKeyException.class)
     public void duplicateKey() {
         dao.deleteAll();
         
