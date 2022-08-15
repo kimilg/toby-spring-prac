d97e4fda4adcbf33031cefbd0737b0f5d07f7df3
ilgoo.kim <ilgoo.kim@navercorp.com>
2022-08-15 19:42:15 +0900

    aop와 tx를 이용하여 advice, pointcut 설정

---
+++ src/main/java/springbook/learningtest/spring/pointcut/Bean.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)Bean.java 2022. 08. 08
+ * <p>
+ * Copyright 2022 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package springbook.learningtest.spring.pointcut;
+
+/**
+ * @author Ilgoo.Kim
+ */
+public class Bean {
+    public void method() throws RuntimeException {
+        
+    }
+}
